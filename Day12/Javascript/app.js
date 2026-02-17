dc6e161ba4439ecefa2b5d0d05764d4e07a8a19f
Prashant Vishwakarma <pbvishwakarma2005@gmail.com>
2026-02-18 00:36:28 +0530

    Add files via upload
    🚀 Day 12/28 of my Web Development Journey with @UnifyLabs!
    Built a multi-page DevConf site focused on accessibility (ARIA + semantic HTML), performance optimization, and responsive Grid/Flex layouts 💻✨
    Learning by building every day! 💪
    
    #WebDevelopment #UnifyLabs #Day12 #Frontend #LearnToCode

---
+++ Day12/Javascript/app.js
@@ -0,0 +1,11 @@
+// Example GPU animation trigger
+
+document.querySelectorAll(".card").forEach(card=>{
+card.addEventListener("mouseenter",()=>{
+card.style.transform="translateY(-8px)";
+});
+
+card.addEventListener("mouseleave",()=>{
+card.style.transform="translateY(0)";
+});
+});
